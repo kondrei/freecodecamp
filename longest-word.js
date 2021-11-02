f3e18fc16fed54aaf4becf45e80dd079af656667
kondrei <kondrei@gmail.com>
2021-11-02 16:13:25 +0200

    longest negativ

--- longest-word.js
+++ longest-word.js
@@ -14,14 +14,10 @@ function largestOfFour(arr) {
   let longest = []
   
 for (let x = 0; x < arr.length; x++) {
-    let max = 0
-    for (let y = 0; y < arr[x].length; y++){
-        if(arr[x][y]>max) 
-          max = arr[x][y];
-    }
-    longest.push(max);
+
+    longest.push(Math.max(...arr[x]))
 }
 return longest
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]])
