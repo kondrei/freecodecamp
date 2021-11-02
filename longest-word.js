c40c09064151b78665a54d5325e0e331326e5327
kondrei <kondrei@gmail.com>
2021-11-02 16:01:11 +0200

    test

--- longest-word.js
+++ longest-word.js
@@ -10,3 +10,18 @@ function findLongestWordLength(str) {
 
 findLongestWordLength("What if we try a super-long word such as otorhinolaryngology");
 
+function largestOfFour(arr) {
+  let longest = []
+  
+for (let x = 0; x < arr.length; x++) {
+    let max = 0
+    for (let y = 0; y < arr[x].length; y++){
+        if(arr[x][y]>max) 
+          max = arr[x][y];
+    }
+    longest.push(max);
+}
+return longest
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
